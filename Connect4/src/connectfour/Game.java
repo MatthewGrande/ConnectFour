2334aa71afc5bcff8b1ec3dad69ac006c841a788
Matthew Grande <matthew@Archon.local>
2018-05-25 03:36:44 -0400

    Added TODOs, cleaned code

--- Connect4/src/connectfour/Game.java
+++ Connect4/src/connectfour/Game.java
@@ -13,7 +13,7 @@ public class Game {
 		c.print();
 		int columnPlayed = 3; int player;
 
-		/* first move for player 1 (played by computer) : CURRENTLY: randomly placed on the board
+		/*
 		 *TODO: allow the player to decide who starts
 		 *( OR: flip a coin to determine who starts)
 		 *I want to see the coin tossing process! 
@@ -21,28 +21,40 @@ public class Game {
 		 * "the coin landed on heads! you decide who starts!,etc)
 		 */
 
+		//first move for player 1 (played by computer) : CURRENTLY: randomly placed on the board
 		c.addDisk(firstMovePlayer1(), 1);
 		c.print(); //Print out the board
 		int nbTurn = 1;
 
-		while (nbTurn < 42){ // maximum of turns allowed by the size of the grid
+		//while (c.spaceLeft) // While there is space on the board
+		
+		while (nbTurn < 42) { // maximum of turns allowed by the size of the grid
+		
 			player = nbTurn %2 + 1; //Determine which player gets to play.
+			//TODO: this ^ will change once we find out who starts.
 			if (player == 2){ 
 				columnPlayed = getNextMove(keyboard, c, 2);
 			}
+			
 			if (player == 1){
 				columnPlayed = movePlayer1(columnPlayed, c);
 			}
 
-			System.out.println(columnPlayed);
+			System.out.println(columnPlayed); // GOING TO REMOVE THIS ONCE I GET A NICE BOARD
 			c.addDisk(columnPlayed, player);
-			c.print(); //Print out the board
+			c.print(); //Print out the board, GOING TO REMOVE THIS ONCE I GET A NICE BOARD.
+			
 			if (c.isWinning(columnPlayed, player)){
 				c.print();
 				System.out.println("Congrats to player " + player + " !");
 				return(player);
 			}
+			
 			nbTurn++;
+			
+			//if (nbTurn == 42)
+			//	c.spaceLeft = false;
+			// System.out.println("The game has ended in a draw!");
 		}
 		return -1;
 	}
@@ -75,7 +87,8 @@ public class Game {
 	}
 
 	public static int firstMovePlayer1 (){
-		return move.nextInt(7); // The AI starts at a random place on the board
+		//return move.nextInt(7); // The AI starts at a random place on the board
+		return 3; //The AI starts by placing his piece in the middle of the board(it is the optimal position).
 	}
 
 	public static int movePlayer1 (int columnPlayed2, Configuration c){
@@ -85,12 +98,16 @@ public class Game {
 		int i=1;
 		if (c.canWinNextRound(1) != -1) // if the AI can win next turn, he will
 			return c.canWinNextRound(1);
+		
 		else if (c.canWinNextRound(2) != -1) // else if the human can win next turn, the AI will stop him.
 			return c.canWinNextRound(2);
+		
 		else if (c.canWinTwoTurns(1) != -1)
 			return c.canWinTwoTurns(1); // if the AI can win in two turns, he will
+		
 		else if (c.canWinTwoTurns(2) != -1) // else if the human can win in two turns, the AI will stop him
-			return c.canWinNextRound(2);
+			return c.canWinTwoTurns(2);
+		
 		else if (c.available[last]<=5) // otherwise, he'll put a piece on top of yours.
 			return last;
 
